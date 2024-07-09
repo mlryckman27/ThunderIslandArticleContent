453e740c76c3e8571d1d3da4f5a4af25a2c96715
mlryckman27 <mlryckman27@gmail.com>
2024-07-08 21:27:36 -0400

    Added DependencyInjection article content.

---
+++ DependencyInjection/Program.cs
@@ -0,0 +1,14 @@
+using DependencyInjection;
+using DependencyInjection.Services;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddHttpClient<TodoService>(client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
---
+++ DependencyInjection/Services/TodoService.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Http.Json;
+
+namespace DependencyInjection.Services
+{
+	public class TodoService
+	{
+		private readonly HttpClient _httpClient;
+
+		public TodoService(HttpClient httpClient)
+		{
+			_httpClient = httpClient;
+		}
+
+		public async Task<TodoItem[]> GetTodosAsync()
+		{
+			return await _httpClient.GetFromJsonAsync<TodoItem[]>("sample-data/todos.json");
+		}
+	}
+
+	public class TodoItem
+	{
+		public string? Title { get; set; }
+		public string? Description { get; set; }
+		public bool IsDone { get; set; }
+	}
+}
